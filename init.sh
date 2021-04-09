c4ceaeb7ea1810e3076fb5a922c860d0f6db9bdd
Kevin Charecki <kevin.charecki@gmail.com>
2021-04-09 07:49:03 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,3 +5,16 @@ sudo apt update
 # Screenshoot utility - better than lightshot
 # Add 'flameshot gui' as custom printscreen shortcut
 sudo apt install flameshot
+
+# Anki
+sudo apt install libxcb-xinerama0
+tar xjf Downloads/anki-2.1.43-linux.tar.bz2
+cd anki-2.1.43-linux
+sudo ./install.sh
+
+# Brave
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
