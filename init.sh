6636810b7986cf8195add5ccf3a97ccd5b445880
Kevin Charecki <kevin.charecki@gmail.com>
2021-04-09 07:43:34 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo apt update
+
 # Screenshoot utility - better than lightshot
 # Add 'flameshot gui' as custom printscreen shortcut
-apt install flameshot
+sudo apt install flameshot
